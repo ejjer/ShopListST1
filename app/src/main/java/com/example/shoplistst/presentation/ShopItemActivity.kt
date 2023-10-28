c517f298de0589fe0b431f88882caa9e2ee12a77
ThinkPad T470 <belkoegor1@gmail.com>
2023-10-28 22:21:30 +0300

    ShopItemViewModel

--- app/src/main/java/com/example/shoplistst/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoplistst/presentation/ShopItemActivity.kt
@@ -8,6 +8,7 @@ import com.example.shoplistst.databinding.ActivityShopItemBinding
 class ShopItemActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityShopItemBinding
+    private lateinit var viewModel: ShopItemViewModel
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
