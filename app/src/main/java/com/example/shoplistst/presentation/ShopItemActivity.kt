b41a03118c75f830a9f94f0550eae70b832060a0
ThinkPad T470 <belkoegor1@gmail.com>
2023-10-28 18:12:11 +0300

    layout

---
+++ app/src/main/java/com/example/shoplistst/presentation/ShopItemActivity.kt
@@ -0,0 +1,17 @@
+package com.example.shoplistst.presentation
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shoplistst.databinding.ActivityShopItemBinding
+
+class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityShopItemBinding
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        binding = ActivityShopItemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
